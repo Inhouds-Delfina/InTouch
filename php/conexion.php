42ebc676aebed23ffe2e78817d8a3e11e685532d
tatalachispa <isaias.rojas13raicloud.com>
2025-09-30 18:40:31 -0300

    arreglo

--- php/conexion.php
+++ php/conexion.php
@@ -1,21 +1,25 @@
+$conn->set_charset("utf8mb4_unicode_ci");
+?>
 <?php
+
 ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
 
-$host = "https://auth-db1659.hstgr.io/";       
-$user = "u214138677_intouch";      
-$pass = "Lachispa@25";     
-$dbname = "u214138677_intouch";       
+$host = "auth-db1659.hstgr.io"; 
+$user = "u214138677_intouch";      
+$pass = "Lachispa@25";     
+$dbname = "u214138677_intouch";       
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
-  die(json_encode([
-    "status" => "error",
-    "msg" => "Error de conexión: " . $conn->connect_error
-  ]));
+ 
+  die(json_encode([
+    "status" => "error",
+    "msg" => "Error de conexión: " . $conn->connect_error
+  ]));
 }
 
-$conn->set_charset("utf8mb4_unicode_ci");
+$conn->set_charset("utf8mb4"); 
 ?>
