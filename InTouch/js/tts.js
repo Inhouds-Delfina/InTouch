a2861b8f01b1db88a206583313016461ef60d7fa
tatalachispa <isaias.rojas13ra@icloud.com>
2025-10-15 21:02:00 -0300

    Delete InTouch directory

--- InTouch/js/tts.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('speakText')?.addEventListener('click', () => {
-  const text = document.getElementById('freeText').value.trim();
-  speak(text);
-});
-
-document.getElementById('stopSpeech')?.addEventListener('click', () => {
-  window.speechSynthesis.cancel();
-});
